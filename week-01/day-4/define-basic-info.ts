eed138a74fb51c87a48e4816ae8e13a42e952103
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 16:25:24 +0100

    define-basic-info  complete

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,16 @@
+  
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+
+let myName: string = 'Martin Smitnya';
+let myAge: number = 26;
+let myHeight: number = 1.83;
+let isMarried: boolean = false;
+
+console.log(myName, myAge, myHeight, isMarried);
