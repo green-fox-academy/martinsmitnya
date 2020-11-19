24a712b1a76e3c1f5967c45389d5ef227fd64a5c
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 15:58:43 +0100

    BMI complete

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+//BMI Formula: Weight / Height^2
+BMI = massInKg / heightInM** ;
+
+console.log(heightInM)
+console.log (BMI));
