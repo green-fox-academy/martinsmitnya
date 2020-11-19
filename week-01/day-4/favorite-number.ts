7a457532055681d0f315b4df056098440f452527
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 15:24:25 +0100

    favorite-numberf complete

---
+++ week-01/day-4/favorite-number.ts
@@ -0,0 +1,11 @@
+
+'use strict';
+
+let favoriteNumber: number = 8;
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+
+let myFavouriteNumber: number = 21;
+console.log ('My favorite number is: ' + myFavouriteNumber);
