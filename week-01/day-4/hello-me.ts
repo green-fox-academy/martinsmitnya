9cc53ea04d2b236b7862f65fbab4bc521024e487
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 12:49:29 +0100

    typos

--- week-01/day-4/hello-me.ts
+++ week-01/day-4/hello-me.ts
@@ -3,5 +3,5 @@
 // Modify this program to greet you instead of the World!
 console.log('Hello, World!');
 
-let name= 'Martin Smitnya'
+let name: string= 'Martin Smitnya'
 console.log('Hello,' name);
