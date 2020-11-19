13190dfcb13836752250191c2131343066ae8c24
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 17:49:43 +0100

    Conditinal-veriables done

--- week-01/day-4/conditional-variable-mutation.ts
+++ week-01/day-4/conditional-variable-mutation.ts
@@ -71,8 +71,18 @@ let out3: string = '';
 // if d is dividable by 4
 // and time is not more than 200
 // set out3 to "check"
+
 // if time is more than 200
 // set out3 to "Time out"
+
 // otherwise set out3 to "Run Forest Run!"
+
+if (d%4===0 && time<200) {
+  out3= 'check';
+}else if (time> 200){
+  out3= 'Time out';
+}else {
+  out3= 'Run Forest Run!';
+}
 
 console.log(out3);
