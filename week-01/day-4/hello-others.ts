9a2ea0cf5dc46be73b499dd06f8e1384d43b30c7
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-19 13:11:09 +0100

    completed

---
+++ week-01/day-4/hello-others.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+let mate1: string = 'Csabi';
+let mate2: string = 'Éva';
+let mate3: string = 'Gábor';
+
+console.log('Hello ', mate1, '!');
+console.log('Hello ', mate2, '!');
+console.log('Hello ', mate3, '!');
