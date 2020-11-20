01cc9ed99b77cf103b83ad34557cd16951c99c5f
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-20 13:33:14 +0100

    typofix

--- week-01/day-5/pyramid.ts
+++ week-01/day-5/pyramid.ts
@@ -23,11 +23,11 @@ for (let i: number = 0; i < lineCount; i++){      //For each line do this:
   
   
   
-  for (let j: number = 0; j < totalCharacters/2 - i; j++ ){   // print row/2 times " " characters
+  for (let j: number = 0; j < totalCharacters/2 - i; j++ ){   // print currentLine/2 times " " characters
     answer = answer+ ' ';
   } 
   
-  for (let k: number = 0;     k < (i + 1)*2-1 ;      k++) { //add * as many as +2 each lne
+  for (let k: number = 0;     k < (i + 1)*2-1 ;      k++) { //add  The line we are on(i+1) times *2 minus-1'*' 
     answer = answer+ '*';   
   }    
   
