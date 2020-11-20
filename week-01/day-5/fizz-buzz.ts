267e3860d3faa632e114b52e0e2d8b7740f4fa92
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-20 10:55:46 +0100

    Fizz-Buzz easy solution complete

---
+++ week-01/day-5/fizz-buzz.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+//Easy solution:
+
+for (let i: number = 1; i<=100; i++) {
+  if (i % 3===0 && i % 5 ===0) {
+   console.log('FizzBuzz');
+  }else if (i % 3 ===0) {
+    console.log('Fizz');
+  }else if (i % 5 ===0) {
+    console.log('Buzz');;
+  }else {
+    console.log(i);
+  }
+}
+
+
+
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
+for (let i: number = 1; i<=100; i++) {
+  let answer: any = '';
+  //if (i % 3===0 && i % 5 ===0) {
+  //  console.log('FizzBuzz');}
+  if (i % 3 ===0) {
+    answer = answer+ 'Fizz';
+  }else if (i % 5 ===0) {
+    answer = answer+ 'Buzz';
+  }else {
+    answer = answer+ i;
+  }
+  console.log(answer);
+}
