3be2d5952897350405f01d178f8bfa97e3ccb0e3
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-23 15:41:11 +0100

    increment... done

---
+++ week-02/day-01/incrementElement.ts
@@ -0,0 +1,8 @@
+// -  Create a variable named `numbers` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element directly (without loops and without array methods: forEach, map, ...)
+// -  Log the third element to the console
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+console.log('was: ' + numbers[2]);
+numbers[2]++
+console.log(numbers[2]);
