5cc0b731a40834449352549266083ca54ae6f5d7
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-23 18:16:18 +0100

    NOT SOLVED YET

--- week-02/day-01/unique.ts
+++ week-02/day-01/unique.ts
@@ -10,13 +10,17 @@ let numbers:number[] = [1, 11, 34, 11, 52, 61, 1, 34];
 function unique(arr:number[]) {
   let uniquieList:number [] = [];       //This is a list outside the loop wich we retunr at the end of the function
   for (let i:number = 0; i < arr.length; i++){        //go throut elements of arr[i]
-    for (let j:number = uniquieList.length; j >= 0; j--) {     //Go trhout elements of the uniuniquieList
-      if (arr[i] !== uniquieList[j] && arr[i] !== uniquieList [uniquieList.length -1 ]) { //If the arrys element value [i] is not equial to the unique list item
-        uniquieList.push(arr[i]);            // .push the arrays [element] value on the uniq list
-      } 
+    if (arr[i] !== uniquieList[
+      for (let j:number = uniquieList.length; j >= 0; j--) {return  uniquieList [j] }
+    ] { uniquieList.push(arr[i]);}
+
+   //for (let j:number = uniquieList.length; j >= 0; j--) {     //Go trhout elements of the uniuniquieList
+    //  if (arr[i] !== uniquieList[j] && arr[i] !== uniquieList [uniquieList.length -1 ]) { //If the arrys element value [i] is not equial to the unique list item
+    ///   uniquieList.push(arr[i]);            // .push the arrays [element] value on the uniq list
+    //  } 
        
       console.log ('The _J is:  ' + j + 'The i is' + i) 
-    }
+   // }
 
   } 
   console.log ('The list is:  '+ uniquieList)
