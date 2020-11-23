283623b616dadd19ffc46b6df9e758014376cd9c
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-23 16:12:23 +0100

    GreeterFunction... done

---
+++ week-02/day-01/GreeterFunction.ts
@@ -0,0 +1,13 @@
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
+
+
+let nameToGreet: string = 'Green Fox';
+
+function greet(nameToGreet) {
+  return 'Greetings, dear ' + nameToGreet
+}
+
+console.log(greet(nameToGreet));
