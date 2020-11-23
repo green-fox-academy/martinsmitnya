32ae3f7e9bf7e5772d555a26cee64f2c55b5f187
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-23 16:26:13 +0100

    summing function ... done

--- week-02/day-01/summing.ts
+++ week-02/day-01/summing.ts
@@ -1,2 +1,13 @@
 
 // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
+
+function sum (until:number) {
+  let summed:number = 0;
+  for (let i:number = 0; i <= until; i++) {
+    summed += i;
+  }
+  return summed
+}
+
+console.log(sum(10));
