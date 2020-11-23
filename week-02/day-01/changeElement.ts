7280ea16f002a26c79b0e90c82e5f666af36852f
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-23 15:26:31 +0100

    solved with .map static solution

---
+++ week-02/day-01/changeElement.ts
@@ -0,0 +1,17 @@
+// -  Create an array named `numbers` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numbers:number[] = [1, 2, 3, 8, 5, 6];      //I have an array
+
+
+function myFunction (i, x) {            //This fucntion converts arr[i] to value x
+  numbers[i] = x;
+  return numbers
+}
+myFunction(3, 4);   //call the function
+console.log(numbers[3]);
+
+
+numbers.map (function converter() {numbers[3]=99; return numbers}) 
+console.log(numbers[3]);
