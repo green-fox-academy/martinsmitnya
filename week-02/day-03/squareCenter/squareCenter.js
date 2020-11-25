79c31fd3a5c01e16f62b0788b5964de87a3373df
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-25 14:00:06 +0100

    squarecenter... done

---
+++ week-02/day-03/squareCenter/squareCenter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+
+//CODE FROM HERE
+
+
+
+// Draw a green 10x10 square to the center of the canvas.
+
+ctx.strokeStyle= 'red'
+ctx.strokeRect(250, 150, 100, 100);
