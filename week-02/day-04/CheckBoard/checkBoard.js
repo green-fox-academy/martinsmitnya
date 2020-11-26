589dbf3bd704df9fa55c183cafbd1dd879d04a21
Martin Smitnya <smitnyamartin@gmail.com>
2020-11-26 13:09:25 +0100

    checkboard drawn with funkctions

--- week-02/day-04/CheckBoard/checkBoard.js
+++ week-02/day-04/CheckBoard/checkBoard.js
@@ -4,10 +4,33 @@ const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
 //CODE FROM HERE
+//DrawSquare function
+function drawSquare (fromX, fromY) {
+  ctx.fillRect(fromX, fromY, 50, 50);
+}
 
+//DrawOneRow
+function drawRow (row, squares, start) {
+  for (let i =0; i<squares; i++){
+    if (start === 'white'){
+      drawSquare (i*100 + 50, row*50);
+    } else {
+      drawSquare (i*100, row*50);
+    }
+  }
 
+}
 
+//Multiple rows
+function drawCheckBoard (rowTotal, squaresTotal) {
+  for (let i = 0; i < rowTotal; i++){
+    if (i%2 ===0){
+      drawRow (i, squaresTotal, 'white');
+    } else {
+      drawRow (i, squaresTotal, 'black');
+    }
+  }
 
+}
 
-
-ctx.fillRect(10, 10, 100, 100);
+drawCheckBoard(8, 6);
