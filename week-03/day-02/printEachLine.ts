f55841917aa1ef86ee77723917d66c7c5d94a490
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-01 13:31:19 +0100

    print each line works IN JS...done

--- week-03/day-02/printEachLine.ts
+++ week-03/day-02/printEachLine.ts
@@ -1,14 +1,12 @@
 const fs = require('fs');
-
-  
 // Write a program that opens a file called "my-file.txt", then prints
 // each line from the file.
 // If the program is unable to read the file (for example it does not exist),
 // then it should print the following error message: "Unable to read file: my-file.txt"
-try{
-  let fileContent: string = fs.readFileSync('my-file.txt', 'utf-8');
+try {
+    let fileContent = fs.readFileSync('my-file.txt', 'utf-8');
 }
-catch(e) {
-  console.log(e);
-  console.log(`Unable to read file: my-file.txt`);
+catch (e) {
+    console.log(e);
+    console.log("M-m-Morty I'm unable to read <burp> file: my-file.txt");
 }
