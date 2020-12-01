c3878088b68f5a1f8930b8c216e0efa7ea30b850
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-01 17:40:02 +0100

    decripter... done

---
+++ week-03/day-02/doubleDecrypter.ts
@@ -0,0 +1,27 @@
+import { tmpdir, totalmem } from "os";
+
+const fs = require('fs');
+
+
+function decrypter (from:string) {
+  let bufferContent:string = '';
+  let temporaryContent:string = '';
+  try{
+    bufferContent = fs.readFileSync(from, 'utf-8').toString();    //WE parse all content to a string
+  }catch(e) {console.log('False, reading error');}
+  //Loop goes trhou each letter
+  for (let i:number = 0; i<bufferContent.length; i++){
+    if (bufferContent[i] !== bufferContent[i-1]) {
+      temporaryContent +=bufferContent[i];
+    }
+  }
+  console.log('Temporaray content is: ' + temporaryContent);
+
+  try{
+    fs.writeFileSync(from, temporaryContent);
+  }catch(e) {console.log('False, writing error');}
+
+}
+
+decrypter ('multpleLine.txt');
+
