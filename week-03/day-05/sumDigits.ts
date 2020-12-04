a48395cee0ccdbdfc19173a52a1eb6df97c6c55f
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-04 15:40:45 +0100

    sumDigits solved with recrursion string method

--- week-03/day-05/sumDigits.ts
+++ week-03/day-05/sumDigits.ts
@@ -6,29 +6,17 @@ function sumDigit(n:number):number{
   //Base case: if we reached the first digit return its value
   let stringValue:string = n.toString();
   let firstdigit:number = parseInt(stringValue[0]);
+  let currentdigit:number  = parseInt(stringValue[stringValue.length-1])
+  let nextNumber = parseInt( stringValue.slice(0, stringValue.length-1))
 
-
-
-  if ( sumDigit( parseInt( stringValue.slice(0, stringValue.length-1))       ) === firstdigit ){
+  console.log('fulll number is: ' + n +' first is: ' + firstdigit + ' current is: ' + currentdigit + ' nextNumber is ' + nextNumber);
+  /*I tkae the number I got, parse it toString(). Determine the first number, set it to return the IF guard. 
+  Determine the current (last) digit im looking at, and the next number that will be parsed and give it to function*/
+  if ( n  === firstdigit) {
     return firstdigit;
-    // add the current digit AND the last digit together
   } else {
-     return parseInt(stringValue[stringValue.length-1]) sumDigit(parseInt( stringValue.slice(0, stringValue.length-1) ));
+     return currentdigit + sumDigit(nextNumber);
   }
-
 }
 
-console.log(sumDigit(1234));
-
-
-/* TESTING
-let n = 12345
-
-let stringValue:string = n.toString();
-let firstdigit:number = parseInt(stringValue[0]);
-
-let theEND = parseInt( stringValue.slice(0, stringValue.length-1));
-theEND = parseInt( stringValue.slice(0, stringValue.length-1));
-console.log(theEND, typeof theEND);
-
-*/
+console.log(sumDigit(121691));
