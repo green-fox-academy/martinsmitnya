c983c2653d31ad5e6733cfe9ed8b74545176806d
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-04 17:35:10 +0100

    figured out this one too ...done

--- week-03/day-05/strings.ts
+++ week-03/day-05/strings.ts
@@ -19,29 +19,26 @@ console.log(tellMeY('Anx strings guxs'));
 */
 
 
-function tellMeYAgain(convert:string){
+function tellMeYAgain(convert: string) {
   let arr: string[] = convert.split('');
-  let converted: string = '';
-               console.log('THE ARRAY IS: ' + arr);
-               console.log('THE ARRAY length is : ' + arr.length);
-               console.log('LAst element is : ' + arr[arr.length-1]);
-
-  if(arr.length < 2) {
-
-    if (arr[arr.length-1] === 'x') {
+  if (arr.length < 2) {
+    if (arr[arr.length - 1] === 'x') {
       return 'y';
-    }else {
-      return arr[arr.length-1];
-    } 
+    } else {
+      return arr[arr.length - 1];
+    }
 
-
-  
-  } 
-  //Add teh first character AND decreese the length of the array
-  else {
-  return arr[0] + tellMeYAgain(convert.slice(1))
   }
-  
+  //Add the first character AND decreese the length of the array
+  else {
+    if (arr[0] === 'x') {
+      return 'y' + tellMeYAgain(convert.slice(1))
+    } else {
+      return arr[0] + tellMeYAgain(convert.slice(1))
+    }
+
+  }
+
 }
 
 console.log(tellMeYAgain('hello guxz'));
