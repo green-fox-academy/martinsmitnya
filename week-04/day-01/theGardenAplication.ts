6b4b4ca35a93218f7c963acc3463c6f9329dad7c
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-07 16:17:39 +0100

    testing if im ready..

--- week-04/day-01/theGardenAplication.ts
+++ week-04/day-01/theGardenAplication.ts
@@ -7,24 +7,58 @@ eg. watering with 40 and 4 of them need water then each gets watered with 10
 */
 
 class Garden {
-  protected _trees: Tree[];
-  protected _flowers: Flower[];
+  // protected _trees: Tree[];
+  // protected _flowers: Flower[];
+  protected _plants:(Flower | Tree)[];
   protected _name:string;
 
   constructor (name:string) {
     this._name = name ?? 'myDefaultGardenName'
+    this._plants = [];
   }
 
+  /*
   public addTrees (tree:Tree) {
     this._trees.push(tree);
   }
   public addFlowers (flower:Flower) {
     this._flowers.push(flower);
   }
+  */
+ public addPlant (plant: (Flower | Tree)) {
+  this._plants.push(plant);
+}
+
+
 
   public watering (waterAmount:number) {
     console.log(`Watering with ${waterAmount}`);
     //when watering it should only water those what needs water with equally divided amount amongst them
+    
+    // if it does water the plant with some part of the water
+    let waterablePlants: (Tree | Flower)[] = [];
+    //goes throu each FLower OR tree in the garden
+    //checks if the given tree/flower needs water -> pushes it to the watering array
+    for (let i:number = 0; i <this._plants.length; i++){
+      if (this._plants[i].checkWaterLevel() === 1) {
+        waterablePlants.push(this._plants[i]);
+      }else {
+        return this._plants[i].checkWaterLevel;
+      }
+      
+    }
+    for (let i:number = 0; i <waterablePlants.length; i++) {
+      waterablePlants[i].wateringPlant(waterAmount/waterablePlants.length);
+
+      return waterablePlants[i].checkWaterLevel;
+    }
+
+
+    
+
+
+
+
   }
 
 
@@ -47,18 +81,21 @@ class Tree {
     this._waterLevel = 0;
   }
 
-  checkWaterLevelOfTree () {
+  checkWaterLevel():number {
     if (this._waterLevel < 10) {
+      return 1
       console.log(`The ${this._color} Tree needs water`);
     }else {
+      return 0
       console.log(`The ${this._color} Tree doesnt need water`);
     }
   }
 
-  wateringTree(waterAmount:number):void {
+  wateringPlant(waterAmount:number):void {
     this._waterLevel += waterAmount*0.4; 
   }
 }
+
 
 
 
@@ -80,15 +117,17 @@ class Flower {
     this._waterLevel = 0;
   }
   
-  checkWaterLevelOfFlower () {
+  checkWaterLevel ():number {
     if (this._waterLevel < 5) {
+      return 1;
       console.log(`The ${this._color} Flower needs water`);
     }else {
+      return 0;
       console.log(`The ${this._color} Flower doesnt need water`);
     }
   }
   
-  wateringFlower(waterAmount:number):void {
+  wateringPlant(waterAmount:number):void {
     this._waterLevel += waterAmount*0.75; 
   }
 
@@ -109,3 +148,13 @@ tulipFlower.wateringFlower(20);
 pineTree.checkWaterLevelOfTree();
 tulipFlower.checkWaterLevelOfFlower();
 */
+
+
+let garden = new Garden ('BeautifulGarden');
+let pineTree = new Tree ('black');
+let tulipFlower = new Flower('red');
+
+garden.addPlant(pineTree);
+garden.addPlant(tulipFlower);
+
+console.log(garden.watering(20));
