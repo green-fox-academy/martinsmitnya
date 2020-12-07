3dc80c228b25e782e631f662eaa094888232899c
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-07 15:18:46 +0100

    added the trees and teh flower with watering and water cecking functionsn

--- week-04/day-01/theGardenAplication.ts
+++ week-04/day-01/theGardenAplication.ts
@@ -38,6 +38,28 @@ when watering it the tree can only absorb the 40% of the water
 eg. watering with 10 the tree's amount of water should only increase with 4
 */
 
+class Tree {
+  protected _color:string;
+  protected _waterLevel: number;
+
+  constructor (color:string) {
+    this._color = color;
+    this._waterLevel = 0;
+  }
+
+  checkWaterLevelOfTree () {
+    if (this._waterLevel < 10) {
+      console.log(`The ${this._color} Tree needs water`);
+    }else {
+      console.log(`The ${this._color} Tree doesnt need water`);
+    }
+  }
+
+  wateringTree(waterAmount:number) {
+    this._waterLevel += waterAmount*0.4; 
+  }
+}
+
 
 
 /*
@@ -46,3 +68,27 @@ needs water if its current water amount is less then 5
 when watering it the flower can only absorb the 75% of the water
 eg. watering with 10 the flower's amount of water should only increase with 7.5
 */
+
+
+class Flower {
+  protected _color:string;
+  protected _waterLevel: number;
+
+  constructor (color:string) {
+    this._color = color;
+    this._waterLevel = 0;
+  }
+  
+  checkWaterLevelOfFlower () {
+    if (this._waterLevel < 5) {
+      console.log(`The ${this._color} Flower needs water`);
+    }else {
+      console.log(`The ${this._color} Flower doesnt need water`);
+    }
+  }
+  
+  wateringFlower(waterAmount:number) {
+    this._waterLevel += waterAmount*0.75; 
+  }
+
+}
