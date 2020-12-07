d6fa64d70fd1f6392e4d58ffa888b98a016d84e6
Martin Smitnya <smitnyamartin@gmail.com>
2020-12-07 13:25:58 +0100

    Studnet_ready

--- week-04/day-01/greenFoxInheritance.ts
+++ week-04/day-01/greenFoxInheritance.ts
@@ -28,13 +28,69 @@ export class Person {
   }
 
 
-  introduce(){
+  public introduce():void{
     console.log(`Hi, I'm ${this._name}, a ${this._age} year old ${this._gender}.`);
   }
-  getGoal(){
+  public getGoal():void{
     console.log(`My goal is: Live for the moment!`);
   }
 
 
 }
+/* TESTING
+let Sahra: Person = new Person ();
+console.log(Sahra);
+Sahra.getGoal();
+Sahra.introduce();
+*/
+
+
+/*
+Create a Student class that has the same fields and methods as the Person class, and has the following additional
+
+fields:
+previousOrganization: the name of the student’s previous company / school
+skippedDays: the number of days skipped from the course
+methods:
+getGoal(): prints out 'My goal is: Be a junior software developer.'
+introduce(): 'Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already.'
+skipDays(numberOfDays): increases skippedDays by numberOfDays
+The Student class has the following constructors:
+
+Student(name, age, gender, previousOrganization): beside the given parameters, it sets skippedDays to 0
+Student(): sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0
+*/
+
+export class Student extends Person {
+  protected _previousOrganization: string;
+  protected _skippedDays: number;
+
+  constructor(name?:string, age?:number, gender?:string, previousOrganization?:string){
+    super(name, age, gender);
+    this._previousOrganization = previousOrganization ?? 'The School of Life';
+    this._skippedDays = 0;
+
+  }
+
+  public getGoal():void{
+    console.log(`My goal is: Be a junior software developer.`);
+  }
+  public introduce():void{
+    console.log(`Hi, I'm ${this._name}, a ${this._age} year old ${this._gender} from ${this._previousOrganization} who skipped ${this._skippedDays} days from the course already.`);
+  }
+  public skipDays(numberOfDays:number):void{
+    this._skippedDays += numberOfDays;
+  }
+
+
+}
+
+/* TESTING
+let Emir = new Student('Emir', 23, 'male', 'BGE');
+console.log(Emir);
+Emir.introduce();
+Emir.skipDays(3);
+Emir.getGoal();
+console.log(Emir)
+*/
 
