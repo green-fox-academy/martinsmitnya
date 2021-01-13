8edce32e5813aafc03411c4c73a9a2fed9474f4c
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-13 18:22:45 +0100

    parsed one data set

--- week-07/day-03/httpRequests.js
+++ week-07/day-03/httpRequests.js
@@ -2,14 +2,22 @@
 let xhr = new XMLHttpRequest();
 
 let method = 'GET'
-let endPoint = 'https://reqres.in/api/users?page=2';
+let endPoint = 'https://swapi.dev/api/starships/9/'//'https://reqres.in/api/users?page=2';
 
 xhr.open(method, endPoint);
 
 xhr.onload = () => {
-  document.querySelector('body').innerHTML = xhr.response;
-}
+  let data = JSON.parse(xhr.response);
+  let dataKeys = Object.keys(data)
 
+  let parentlist = document.querySelector('#list');
+
+  for (let i = 0; i < dataKeys.length; i++) {
+    let myItem = document.createElement('li');
+    myItem.innerHTML = data[dataKeys[i]];
+    parentlist.appendChild(myItem);
+  }
+}
 //THE SAME AS: 
 // xhr.onreadystatechange = () => {
 //   if (xhr.readyState === 4) {
