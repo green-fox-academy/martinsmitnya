483884735623e10bbe15124855b9224b15f46882
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-13 17:31:41 +0100

    base done

--- week-07/day-03/httpRequests.js
+++ week-07/day-03/httpRequests.js
@@ -2,10 +2,19 @@
 let xhr = new XMLHttpRequest();
 
 let method = 'GET'
-let endPoint = 'https://reqres.in/api/unknown';
+let endPoint = 'https://reqres.in/api/users?page=2';
 
 xhr.open(method, endPoint);
+
 xhr.onload = () => {
-  document.querySelector('body').innerHTML = xhr.response
+  document.querySelector('body').innerHTML = xhr.response;
 }
+
+//THE SAME AS: 
+// xhr.onreadystatechange = () => {
+//   if (xhr.readyState === 4) {
+//     document.querySelector('body').innerHTML = xhr.response;
+//   }
+// }
+
 xhr.send();
