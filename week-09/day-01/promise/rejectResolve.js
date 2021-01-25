db0863c66692fda1046011b583f271b24b54bb5d
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-25 17:32:44 +0100

    reject-resolve done

--- week-09/day-01/promise/rejectResolve.js
+++ week-09/day-01/promise/rejectResolve.js
@@ -3,7 +3,7 @@ let promise = new Promise( (fulfill, reject) => {
   fulfill('I FIRED')
   reject(new Error('I DID NOT FIRE') )
 })
-.then(console.log, onRejected)
+.then( console.log, onRejected)
 
 function onRejected(error) {
   console.log(error.message);
