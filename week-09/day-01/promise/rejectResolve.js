5949b0223d910777ade5da63ada8dbbe626ad79c
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-25 17:19:36 +0100

    reject-resolve done

---
+++ week-09/day-01/promise/rejectResolve.js
@@ -0,0 +1,10 @@
+'use strict'
+let promise = new Promise( (fulfill, reject) => {
+  fulfill('I FIRED')
+  reject(new Error('I DID NOT FIRE') )
+})
+.then(console.log, onRejected)
+
+function onRejected(error) {
+  console.log(error.message);
+}
