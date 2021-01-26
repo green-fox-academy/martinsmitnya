7ec5f4be8d585ea54c49893467ddc57f49ee7edc
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-26 15:39:43 +0100

    I am tested Groot

--- week-09/day-02/iamgroot/test/test-routes.js
+++ week-09/day-02/iamgroot/test/test-routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const request = require('supertest');
-const { expect } = require('chai');
-const app = require('../routes');
+const { expect } = require('chai'); //also npm i mocha
+const app = require('../routes'); //whatever aplication
 
 describe('My tests title: /GET request to /groot?message=totallyNotGroot', () => {
   it('With giving a parameter the status is ok, and the given response is the same as expected', (done) => {
@@ -10,7 +10,7 @@ describe('My tests title: /GET request to /groot?message=totallyNotGroot', () =>
     .get('/groot?message=totallyNotGroot')
     .expect(200)
 
-    .end((err, res) => {
+    .end( (err, res) => {
       expect(err).to.be.null;
       expect(res.body.received).to.equal('totallyNotGroot')
       done();
